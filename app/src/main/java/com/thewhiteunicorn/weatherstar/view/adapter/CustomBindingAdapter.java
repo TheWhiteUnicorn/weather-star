62693e8d8062c9a50ee3d27bb77e8a2695bf7a73
Erashov Denis <thewhiteunicorn99@gmail.com>
2020-01-30 20:27:22 +0300

    Add weather conditions icon

--- app/src/main/java/com/thewhiteunicorn/weatherstar/view/adapter/CustomBindingAdapter.java
+++ app/src/main/java/com/thewhiteunicorn/weatherstar/view/adapter/CustomBindingAdapter.java
@@ -1,8 +1,11 @@
 package com.thewhiteunicorn.weatherstar.view.adapter;
 
 import android.view.View;
+import android.widget.ImageView;
 
 import androidx.databinding.BindingAdapter;
+
+import com.squareup.picasso.Picasso;
 
 public class CustomBindingAdapter {
     @BindingAdapter("visibleGone")
@@ -13,5 +16,10 @@ public class CustomBindingAdapter {
     @BindingAdapter("isSelected")
     public static void setSelected(View view, boolean selected) {
         view.setSelected(selected);
+    }
+
+    @BindingAdapter({"weatherCondIconCode"})
+    public static void loadImage(ImageView view, String code) {
+        Picasso.get().load(String.format("http://openweathermap.org/img/wn/%s@2x.png", code)).fit().into(view);
     }
 }
